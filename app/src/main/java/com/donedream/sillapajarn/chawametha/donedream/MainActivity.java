d62016fcf062cb2fbe18ccd7dc34dfb129424861
kan555 <kan-sign-in@Hotmail.com>
2016-02-25 11:18:35 +0700

    design UI Signup finished

--- app/src/main/java/com/donedream/sillapajarn/chawametha/donedream/MainActivity.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/donedream/MainActivity.java
@@ -1,7 +1,9 @@
 package com.donedream.sillapajarn.chawametha.donedream;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(this);
 
     }   //main method
+
+    public void clickSignUp(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+
+    }
 
 }   // main class
