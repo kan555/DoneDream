7ec5f2fa6c4f0866382138ec708f1ee8a831e5fc
kan555 <kan-sign-in@Hotmail.com>
2016-02-25 13:12:15 +0700

    get values from date picker for signup finished

--- app/src/main/java/com/donedream/sillapajarn/chawametha/donedream/SignUpActivity.java
+++ app/src/main/java/com/donedream/sillapajarn/chawametha/donedream/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.donedream.sillapajarn.chawametha.donedream;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -33,6 +34,8 @@ public class SignUpActivity extends AppCompatActivity {
         passwordEditText = (EditText) findViewById(R.id.editText4);
         nameEditText = (EditText) findViewById(R.id.editText5);
         emailEditText = (EditText) findViewById(R.id.editText6);
+        birthDatePicker = (DatePicker) findViewById(R.id.datePicker);
+
 
     }   //blind widget
 
@@ -48,16 +51,27 @@ public class SignUpActivity extends AppCompatActivity {
         if (checkSpace()) {
             //have space
             MyAlertDialog myAlertDialog = new MyAlertDialog();
-            myAlertDialog.myDialog(SignUpActivity.this, "Have Space", "Please, fill in all forms");
+            myAlertDialog.myDialog(SignUpActivity.this, "Alert!", "Please, fill in all forms");
 
         } else {
             // no space
+            gerValueFromDatePicker();
 
 
         }   //if
 
 
     }   //click save data (sign up)
+
+    private void gerValueFromDatePicker() {
+
+        int intDay = birthDatePicker.getDayOfMonth();
+        int intMonth = birthDatePicker.getMonth() +1; //1==jan
+        int intYear = birthDatePicker.getYear();
+
+        Log.d("25Feb", "birth ==> " + intDay + "/" + intMonth + "/" + intYear);
+
+    }   // ger value from date picker
 
     private boolean checkSpace() {
 
